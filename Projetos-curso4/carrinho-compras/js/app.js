8fd2d3bff3e0afadf32b0e6cc51ea7a67fc33e48
Kadu <154840239+Kadu-77@users.noreply.github.com>
2026-05-13 10:13:24 -0300

    Add files via upload

--- Projetos-curso4/carrinho-compras/js/app.js
+++ Projetos-curso4/carrinho-compras/js/app.js
@@ -4,21 +4,39 @@ limpar();
 function adicionar() {
     // Recuperar valores nome, quantidade e valor
     let produto = document.getElementById('produto').value;
+    let quantidade = document.getElementById('quantidade').value;
+
+    if (!produto || produto.trim() === "") {
+        alert("Selecione um produto válido.");
+        return;
+    }
+
+
+    if (isNaN(quantidade) || quantidade <= 0) {
+        alert("Insira uma quantidade válida.");
+        return;
+    }
+
+
     let nomeProduto = produto.split('-')[0];
     let valorUnitario = produto.split('R$')[1];
-    let quantidade = document.getElementById('quantidade').value;
     let preco = quantidade * valorUnitario;
+
     // adicionat no carrinho
+
     let carrinho = document.getElementById('lista-produtos');
     carrinho.innerHTML = carrinho.innerHTML + ` <section class="carrinho__produtos__produto">
           <span class="texto-azul">${quantidade}</span> ${nomeProduto} <span class="texto-azul">R$${valorUnitario}</span>
         </section>`;
+
     // atualizar o valor total
+
    totalGeral = totalGeral + preco;
     let campoTotal = document.getElementById('valor-total'); 
     campoTotal.textContent = `R$ ${totalGeral}`;
     document.getElementById('quantidade').value = 0;
 }
+
 
 function limpar() {
     totalGeral = 0;
